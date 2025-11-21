b729f0df68e3e11aef7c7f9c7583e2f8b2adb6ec
alwingodly <alwinaswin123@gmail.com>
2025-11-21 05:54:44 +0530

    ready for deployment

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { loginController, logoutController, signUpController } from "../controllers/auth.controller.js"
 const router = express.Router()
-router.post("/" , loginController)
+router.post("/sign-in" , loginController)
 router.post("/sign-up" , signUpController)
 router.post("/logout" , logoutController)
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,19 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import { connectDB } from "./config/db.js";
+import path from "path"
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 app.use("/api/auth" , authRoutes)
 app.use("/api/message" , messageRoutes)
-
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname , "../frontend/dist")))
+    app.get("*" , (_ , res)=>{
+        res.sendFile(path.join(__dirname , "../frontend" , "dist" , "index.html"))
+    })
+}
 app.listen( 5001 , ()=>{
 console.log(" Server running in port 5001")
 connectDB()
